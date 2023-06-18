bb6e6adb985072471e3688fcb4038f986117b5ca
Emanuel Jacinto <115151063+ElzenDev@users.noreply.github.com>
2023-06-18 18:06:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-//                ***FUNÇÕES***
 
-function contar(){
+const chk = document.getElementById('chk')
+const ger = document.getElementById('gerador')
+const ball = document.getElementById('ball')
+const label = document.getElementById('label')
+const button = document.getElementById('Sortear')
+let btn = document.getElementById('button')
+let res = document.getElementById('res')
+
+chk.addEventListener('change', ()=> {
+    document.body.classList.toggle('dark')
+    ger.classList.toggle('dark')
+    ball.classList.toggle('dark')
+    label.classList.toggle('dark')
+    btn.classList.toggle('dark')
+    res.classList.toggle('dark')
+})
+
+//                **SORTEIO DE NÚMEROS***
+
+function sortear(){
 
     //      ** VARIAVEIS **
     var maxR = document.getElementById('max_range')
@@ -8,32 +26,41 @@ function contar(){
 
     var minR = document.getElementById('min_range')
     var nMin = Number(minR.value)
-    
-    var btn = document.getElementById('button')
 
-    var res = document.getElementById('res')
-
-
-    var sorteio = Math.floor(Math.random() * (nMax - nMin) + nMin); // CALCULO DO NUMERO
+    if(nMax > nMin){
+        var sorteio = Math.floor(Math.random() * ((nMax + 1) - nMin) + nMin ); // CALCULO DO NUMERO
 
     
-    res.innerHTML = 'O resultado é   ' + sorteio
+        res.innerHTML = sorteio
 
-//     **Verificações pós contagem**
+        //     **Verificações pós contagem**
 
-    if(res.innerHTML = 'O resultado é: ' + sorteio){
-        btn.value = 'Resortear'
-    }
+        if(res.innerHTML = sorteio){
+            btn.value = 'Resortear'
+        }
 
-    if(btn.value = 'Resortear'){
-        btn.addEventListener('click', function(){
-            res.innerHTML = 'O resultado é: ' + sorteio 
-            
-        })
-    }
-    //console.log(sorteio)
+        if(btn.value = 'Resortear'){
+            btn.addEventListener('click', function(){
+                res.innerHTML = sorteio 
+                
+            })
+        }
+        //console.log(sorteio)
+        
+
+        return Number(res)
+        }
+
+    else if(nMax < nMin){
+            window.alert('[ERROR] O Valor Máximo deve ser maior do que o Valor Minímo')
+        }
+        
+    else{
+            window.alert('[ERROR] O Valores devem ser diferentes')
+        }
     
 
-    return Number(res)
+
+    
     
 }
